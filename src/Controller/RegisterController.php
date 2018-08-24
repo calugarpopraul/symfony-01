1d94a4a437a3a0a279d06b3836c1169e16af5bad
user <calugarpopraul@gmail.com>
2018-08-24 21:33:59 +0300

    Login and Register done

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 8/24/18
+ * Time: 4:36 PM
+ */
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends Controller
+{
+    /**
+     * @Route("/register", name="user_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword(
+                $user,
+                $user->getPlainPassword());
+            $user->setPassword($password);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->redirectToRoute('micro_post_index');
+
+        }
+
+        return $this->render(
+            'register/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+}
